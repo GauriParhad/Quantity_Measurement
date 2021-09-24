aa4e274bcfe2ee62d45e0725012d01e86a470ce6
Gaurip10 <thegauriparhad@gmail.com>
2021-09-24 08:54:59 +0530

    Added: Yard To Feet comparison Test

--- src/test/java/com/bridgelabz/FeetInchComparsionTest.java
+++ src/test/java/com/bridgelabz/FeetInchComparsionTest.java
@@ -3,7 +3,7 @@ package com.bridgelabz;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class QuantityTest {
+public class FeetInchComparsionTest {
     @Test
     void given0FeetAnd0Feet_ShouldReturnEqual() {
         Feet feet1 = new Feet(0.0);
@@ -15,6 +15,7 @@ public class QuantityTest {
     void given0FeetAnd1Feet_ShouldReturnNotEqual() {
         Feet feet1 = new Feet(0.0);
         Feet feet2 = new Feet(1.0);
+
         Assertions.assertNotEquals(feet1, feet2);
     }
 
@@ -28,15 +29,15 @@ public class QuantityTest {
     @Test
     void givenFeetAndFeetFromSameRef_WhenEqual_ShouldReturnTrue() {
         Feet feet1 = new Feet(0.0);
-        boolean actual = feet1 == feet1;    // '==' operator is for ref obj
+        boolean actual = feet1 == feet1;
         Assertions.assertTrue(actual);
     }
 
     @Test
-    void givenFeetAndFeetFromDiffRef_WhenNotEqual_ShouldReturnFalse() {
+    void givenFeetAndFeetFromDiffReference_WhenNotEqual_ShouldReturnFalse() {
         Feet feet1 = new Feet(0.0);
         Feet feet2 = new Feet(0.0);
-        boolean actual = feet1 == feet2;   // '==' operator is for ref obj
+        boolean actual = feet1 == feet2;
         Assertions.assertFalse(actual);
     }
 
@@ -48,24 +49,22 @@ public class QuantityTest {
     }
 
     @Test
-    void givenInchAndInchFromSameRef_WhenEqual_ShouldReturnTrue() {
+    void givenInchAndInchFromSameReference_WhenEqual_ShouldReturnTrue() {
         Inch inch1 = new Inch(0.0);
-        boolean actual = inch1 == inch1;    // '==' operator is for ref obj
-        Assertions.assertTrue(actual);
+        boolean actual = inch1 == inch1;
     }
-
     @Test
-    void givenInchAndInchFromDiffRef_WhenNotEqual_ShouldReturnFalse() {
+    void givenInchAndInchFromDiffReference_WhenNotEqual_ShouldReturnFalse() {
         Inch inch1 = new Inch(0.0);
         Inch inch2 = new Inch(0.0);
-        boolean actual = inch1 == inch2;   //for ref o
+        boolean actual = inch1 == inch2;
         Assertions.assertFalse(actual);
     }
 
     @Test
-    void givenFeetAndInchFromDiffType_ShouldReturnNotEqual() {
-        Feet feet = new Feet(0.0);
-        Inch inch = new Inch(0.0);
+    void given1FeetAnd12InchFromDiffType_ShouldReturnNotEqual() {
+        Feet feet = new Feet(1.0);
+        Inch inch = new Inch(12.0);
         Assertions.assertNotEquals(feet, inch);
     }
 
@@ -81,5 +80,32 @@ public class QuantityTest {
         Inch inch1 = new Inch(0.0);
         Inch inch2 = new Inch(1.0);
         Assertions.assertNotEquals(inch1, inch2);
+    }
+
+    @Test
+    void givenFeetAndInch_WhenEqual_ShouldReturnTrue() {
+        Feet feetToInch = new Feet();
+        boolean result = feetToInch.lengthComparison(1.0, 12.0);
+        Assertions.assertTrue(result);
+    }
+    @Test
+    void givenFeetAndInch_WhenNotEqual_ShouldReturnFalse() {
+        Feet feetToInch = new Feet();
+        boolean result = feetToInch.lengthComparison(3.0, 10.0);
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    void givenInchAndFeet_WhenEqual_ShouldReturnTrue() {
+        Inch InchToFeet = new Inch();
+        boolean result = InchToFeet.lengthComparison(12.0, 1);
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    void givenInchAndFeet_WhenNotEqual_ShouldReturnFalse() {
+        Inch InchToFeet = new Inch();
+        boolean result = InchToFeet.lengthComparison(22.0, 3);
+        Assertions.assertFalse(result);
     }
 }
